bb98f6234640e715ebf64d208bf64d43724eec12
Kristjan-Ai <kristjan.arusalu001@gmail.com>
2023-05-12 08:20:17 +0300

    iii

--- parim_wordle_universumis/wordle/views.py
+++ parim_wordle_universumis/wordle/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.template import Library
 from Sõnade_loend.valmis_sonad import sonastik
+import random as r
 
 # Create your views here.
 
 def pealeht(request):
+    sonade_arv = len(sonastik)
     context = {
         "sonad":sonastik
     }
