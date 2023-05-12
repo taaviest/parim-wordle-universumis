772d93f355468f25d3c27579d4c6dbc9210eaf44
Kristjan-Ai <kristjan.arusalu001@gmail.com>
2023-05-12 08:53:39 +0300

    23

--- parim_wordle_universumis/wordle/views.py
+++ parim_wordle_universumis/wordle/views.py
@@ -6,9 +6,20 @@ import random as r
 # Create your views here.
 
 def pealeht(request):
-    sonade_arv = len(sonastik)
+    sonade_arv = len(sonastik)-1
+    mitmes = r.randint(0, sonade_arv)
+    tahed = sonastik[list(sonastik.keys())[mitmes]]
+    taht1 = tahed[0]
+    taht2 = tahed[1]
+    taht3 = tahed[2]
+    taht4 = tahed[3]
+    taht5 = tahed[4]
     context = {
-        "sonad":sonastik
+        "I_taht":taht1,
+        "II_taht":taht2,
+        "III_taht":taht3,
+        "IV_taht":taht4,
+        "V_taht":taht5,
     }
     return render(request, "wordle/pealeht.html", context)
 
