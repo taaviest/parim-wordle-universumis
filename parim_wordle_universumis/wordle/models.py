f56adb9c71d8a519708e10fadc05c6b1aa603148
Kristjan-Ai <kristjan.arusalu001@gmail.com>
2023-05-19 14:36:43 +0300

    model ja view'de uuendused

--- parim_wordle_universumis/wordle/models.py
+++ parim_wordle_universumis/wordle/models.py
@@ -1,4 +1,12 @@
 from django.db import models
+import django
 
 # Create your models here.
 
+class mang(models.Model):
+    oige_sona = models.CharField(max_length=200)
+    sona1 = models.CharField(max_length=200)
+    sona2 = models.CharField(max_length=200)
+    sona3 = models.CharField(max_length=200)
+    sona4 = models.CharField(max_length=200)
+    sona5 = models.CharField(max_length=200)
--- parim_wordle_universumis/wordle/views.py
+++ parim_wordle_universumis/wordle/views.py
@@ -1,35 +1,35 @@
-from django.shortcuts import render
+from urllib.request import HTTPRedirectHandler
+from django.shortcuts import HttpResponse, HttpResponseRedirect, render
 from django.template import Library
-
+from wordle.models import mang
+from django.urls import reverse
 from Sõnade_loend.valmis_sonad import sonastik
 import random as r
 
 # Create your views here.
 
 def algus(request):
+    global mangu_id
     sonade_arv = len(sonastik)-1
     mitmes = r.randint(0, sonade_arv)
-    global oige_sona
     oige_sona  = list(sonastik.keys())[mitmes]
+    uus_mang = mang(oige_sona=oige_sona, sona1="", sona2="",sona3="",sona4="",sona5="")
+    uus_mang.save()
+    mangu_id = uus_mang.id
     print(oige_sona)
-    """    taht1 = tahed[0]
-    taht2 = tahed[1]
-    taht3 = tahed[2]
-    taht4 = tahed[3]
-    taht5 = tahed[4]
-    context = {
-        "I_taht":taht1,
-        "II_taht":taht2,
-        "III_taht":taht3,
-        "IV_taht":taht4,
-        "V_taht":taht5,
-    }"""
     return render(request, "wordle/pealeht.html")
-def kontroll(request):
-    global oige_sona
-    sona = request.POST.get("taht1")+request.POST.get("taht2")+request.POST.get("taht3")+request.POST.get("taht4")+request.POST.get("taht1")
-    if sona == oige_sona:
-        return render(request, "wordle/pealeht.html", {"varv":"green"})
-    else:
-        return render(request, "wordle/pealeht.html", {"varv":"red", "sonum":"Proovi uuesti!"})
 
+def kontroll(request):
+    if request.method=="POST":
+        global mangu_id
+        sona = request.POST.get("taht1")+request.POST.get("taht2")+request.POST.get("taht3")+request.POST.get("taht4")+request.POST.get("taht5")
+        mangu_objekt = mang.objects.get(id=mangu_id)
+        oige_sona = mangu_objekt.oige_sona
+        if sona == oige_sona:
+            sonum="Oige!"
+            return render(request, "wordle/pealeht.html", {"varv":"green", "sonum":sonum, "kas":True})
+            mangu_objekt.delete()
+        else:
+            return render(request, "wordle/pealeht.html", {"varv":"red", "sonum":"Proovi uuesti!", "kas":False})
+    elif request.method=="GET":
+        return HttpResponseRedirect(reverse("algus"))
