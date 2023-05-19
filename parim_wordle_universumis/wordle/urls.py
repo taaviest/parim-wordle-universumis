670f1c6464bace50fe39020e78a0cdd4b7ba3a0c
Kristjan-Ai <kristjan.arusalu001@gmail.com>
2023-05-19 08:09:45 +0300

    Vaga hasti

--- parim_wordle_universumis/wordle/urls.py
+++ parim_wordle_universumis/wordle/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = {
-    path("", views.pealeht, name="pealeht"),
+    path("", views.algus, name="algus"),
+    path("kontroll/", views.kontroll, name="kontroll"),
 }
--- parim_wordle_universumis/wordle/views.py
+++ parim_wordle_universumis/wordle/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render
 from django.template import Library
+
 from Sõnade_loend.valmis_sonad import sonastik
 import random as r
 
 # Create your views here.
 
-def pealeht(request):
+def algus(request):
     sonade_arv = len(sonastik)-1
     mitmes = r.randint(0, sonade_arv)
-    tahed = sonastik[list(sonastik.keys())[mitmes]]
-    taht1 = tahed[0]
+    global oige_sona
+    oige_sona  = list(sonastik.keys())[mitmes]
+    print(oige_sona)
+    """    taht1 = tahed[0]
     taht2 = tahed[1]
     taht3 = tahed[2]
     taht4 = tahed[3]
@@ -20,6 +23,13 @@ def pealeht(request):
         "III_taht":taht3,
         "IV_taht":taht4,
         "V_taht":taht5,
-    }
-    return render(request, "wordle/pealeht.html", context)
+    }"""
+    return render(request, "wordle/pealeht.html")
+def kontroll(request):
+    global oige_sona
+    sona = request.POST.get("taht1")+request.POST.get("taht2")+request.POST.get("taht3")+request.POST.get("taht4")+request.POST.get("taht1")
+    if sona == oige_sona:
+        return render(request, "wordle/pealeht.html", {"varv":"green"})
+    else:
+        return render(request, "wordle/pealeht.html", {"varv":"red", "sonum":"Proovi uuesti!"})
 
