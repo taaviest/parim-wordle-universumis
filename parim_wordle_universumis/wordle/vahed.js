f2387bc84fd322195cd5e7dc943c061ff1b64273
marrga <margaret.filippov@real.edu.ee>
2023-05-26 09:31:04 +0300

    4drr

--- parim_wordle_universumis/wordle/vahed.js
+++ parim_wordle_universumis/wordle/vahed.js
@@ -1,34 +1,34 @@
-var myInputs = document.getElementsByTagName("input");
-var myEditable = document.getElementById("seventh");
-for (var i = 0; i < myInputs.length; i++) {
-  myInputs[i].addEventListener("click", function() {
-    document.getElementById("seventh").focus();
-  })
-}
+<script language="javascript" type="text/javascript">
 
-myEditable.addEventListener("keydown", function(evt) {
-  /****
-   *  A few things are handled here: we can check if
-   *  the input is a numeric, and we can check if the input
-   *  is a backspace. Nothing else is allowed.
-   ****/
-  if (evt.which == 6) {
-    // If a backspace has been pressed, move all the input
-    //  values one field UP.
-    myEditable.blur();
-    for (var i = myInputs.length - 1; i >= 1; i--) {
-      myInputs[i].value = myInputs[i - 1].value;
-    }
-    myInputs[0].value = "";
-  } else if (evt.which >= 48 && evt.which <= 59) {
-    // Here, we have a number. Everything gets bumped to the LEFT
-    if (myInputs[0].value == "") {
-      for (var i = 0; i < myInputs.length - 1; i++) {
-        myInputs[i].value = myInputs[i + 1].value;
-      }
-      myEditable.value = String.fromCharCode(evt.which);
-    }
-  } else {
-    console.log("You did something else...");
+function fnMoveItems(lstbxFrom,lstbxTo)
+{
+ var varFromBox = document.all(lstbxFrom);
+ var varToBox = document.all(lstbxTo); 
+ if ((varFromBox != null) && (varToBox != null)) 
+ { 
+  if(varFromBox.length < 1) 
+  {
+   alert('There are no items in the source ListBox');
+   return false;
   }
-})
+  if(varFromBox.options.selectedIndex == -1) // when no Item is selected the index will be -1
+
+  {
+   alert('Please select an Item to move');
+   return false;
+  }
+  while ( varFromBox.options.selectedIndex >= 0 ) 
+  { 
+   var newOption = new Option(); // Create a new instance of ListItem 
+
+   newOption.text = varFromBox.options[varFromBox.options.selectedIndex].text; 
+   newOption.value = varFromBox.options[varFromBox.options.selectedIndex].value; 
+   varToBox.options[varToBox.length] = newOption; //Append the item in Target Listbox
+
+   varFromBox.remove(varFromBox.options.selectedIndex); //Remove the item from Source Listbox 
+
+  } 
+ }
+ return false; 
+}
+</script>
