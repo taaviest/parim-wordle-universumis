657a4867ca1b88fdfd1ebf33476006bea900ac33
Kristjan-Ai <kristjan.arusalu001@gmail.com>
2023-06-04 13:45:21 +0300

    Vaga suured muudatused
    Varvidel on modelites oma listid, kogu kontrolli view on suht umber tehtud.
    
    Nuud on lihtsalt vaja template'i muutujad sisse panna ja view's moned muutujad ara muuta.

--- parim_wordle_universumis/wordle/models.py
+++ parim_wordle_universumis/wordle/models.py
@@ -5,8 +5,15 @@ import django
 
 class mang(models.Model):
     oige_sona = models.CharField(max_length=200)
-    sona1 = models.JSONField(default=["     ", "white"])
-    sona2 = models.JSONField(default=["     ", "white"])
-    sona3 = models.JSONField(default=["     ", "white"])
-    sona4 = models.JSONField(default=["     ", "white"])
-    sona5 = models.JSONField(default=["     ", "white"])
+    sona1 = models.JSONField(default=list(("", "", "", "", "")))
+    sona1_varv = models.JSONField(default=list(("white", "white", "white", "white", "white")))
+    sona2 = models.JSONField(default=list(("", "", "", "", "")))
+    sona2_varv = models.JSONField(default=list(("white", "white", "white", "white", "white")))
+    sona3 = models.JSONField(default=list(("", "", "", "", "")))
+    sona3_varv = models.JSONField(default=list(("white", "white", "white", "white", "white")))
+    sona4 = models.JSONField(default=list(("", "", "", "", "")))
+    sona4_varv = models.JSONField(default=list(("white", "white", "white", "white", "white")))
+    sona5 = models.JSONField(default=list(("", "", "", "", "")))
+    sona5_varv = models.JSONField(default=list(("white", "white", "white", "white", "white")))
+    def __str__(self):
+        return self.oige_sona+"\n"+self.sona1+"\n"+self.sona1_varv+"\nid: "+self.id
--- parim_wordle_universumis/wordle/views.py
+++ parim_wordle_universumis/wordle/views.py
@@ -13,106 +13,148 @@ def algus(request):
     global mitmes
     mitmes = 1
     sonade_arv = len(sonade_list)-1
-    mitmes = r.randint(0, sonade_arv)
-    oige_sona  = sonade_list[mitmes]
+    mitmes_suvaline = r.randint(0, sonade_arv)
+    oige_sona  = sonade_list[mitmes_suvaline]
     uus_mang = mang(oige_sona=oige_sona)
     uus_mang.save()
     mangu_id = uus_mang.id
-    print(oige_sona)
-    mangu_objekt = uus_mang
-    sona1 = list(mangu_objekt.sona1)
-    sona2 = list(mangu_objekt.sona2)
-    sona3 = list(mangu_objekt.sona3)
-    sona4 = list(mangu_objekt.sona4)
-    sona5 = list(mangu_objekt.sona5)
-    context = {
-            "taht1_1": sona1[0],
-            "taht1_2": sona1[1],
-            "taht1_3": sona1[2],
-            "taht1_4": sona1[3],
-            "taht1_5": sona1[4],
-            "taht2_1": sona2[0],
-            "taht2_2": sona2[1],
-            "taht2_3": sona2[2],
-            "taht2_4": sona2[3],
-            "taht2_5": sona2[4],
-            "taht3_1": sona3[0],
-            "taht3_2": sona3[1],
-            "taht3_3": sona3[2],
-            "taht3_4": sona3[3],
-            "taht3_5": sona3[4],
-            "taht4_1": sona4[0],
-            "taht4_2": sona4[1],
-            "taht4_3": sona4[2],
-            "taht4_4": sona4[3],
-            "taht4_5": sona4[4],
-            "taht5_1": sona5[0],
-            "taht5_2": sona5[1],
-            "taht5_3": sona5[2],
-            "taht5_4": sona5[3],
-            "taht5_5": sona5[4],
-        }
-    return render(request, "wordle/pealeht.html", context)
+    print(uus_mang)
+    return render(request, "wordle/pealeht.html")
 
 def kontroll(request):
     if request.method=="POST":
         global mitmes
         global mangu_id
+        print(mitmes)
         sona = request.POST.get("taht1")+request.POST.get("taht2")+request.POST.get("taht3")+request.POST.get("taht4")+request.POST.get("taht5")
         mangu_objekt = mang.objects.get(id=mangu_id)
         oige_sona = mangu_objekt.oige_sona
+        #salvesta sona vastavalt mitmes on ning leia oiged varvid kastide jaoks
         if mitmes == 1:
-            mangu_objekt.sona1 = sona
-            mangu_objekt.save()
+            if sona in sonade_list:
+                mangu_objekt.sona1 = list(sona)
+                mangu_objekt.save()
+                mitmes_taht = 0
+                for taht in mangu_objekt.sona1:
+                    if taht == list(oige_sona)[mitmes_taht]:
+                        (mangu_objekt.sona1_varv).insert(mitmes_taht, "green")
+                        mangu_objekt.save()
+                        mitmes_taht += 1
+                    elif taht in list(oige_sona):
+                        (mangu_objekt.sona1_varv).insert(mitmes_taht, "yellow")
+                        mangu_objekt.save()
+                        mitmes_taht += 1
+                    else:
+                        (mangu_objekt.sona1_varv).insert(mitmes_taht, "grey")
+                        mangu_objekt.save()
+                        mitmes_taht += 1
+            else:
+                sona_pole = True
         elif mitmes == 2:
-            mangu_objekt.sona2 = sona
-            mangu_objekt.save()
+            if sona in sonade_list:
+                mangu_objekt.sona2 = list(sona)
+                mangu_objekt.save()
+                mitmes_taht = 0
+                for taht in mangu_objekt.sona2:
+                    if taht == list(oige_sona)[mitmes_taht]:
+                        (mangu_objekt.sona2_varv).insert(mitmes_taht, "green")
+                        mangu_objekt.save()
+                        mitmes_taht += 1
+                    elif taht in list(oige_sona):
+                        (mangu_objekt.sona2_varv).insert(mitmes_taht, "yellow")
+                        mangu_objekt.save()
+                        mitmes_taht += 1
+                    else:
+                        (mangu_objekt.sona2_varv).insert(mitmes_taht, "grey")
+                        mangu_objekt.save()
+                        mitmes_taht += 1
+            else:
+                sona_pole = True
         elif mitmes == 3:
-            mangu_objekt.sona3 = sona
-            mangu_objekt.save()
+            if sona in sonade_list:
+                mangu_objekt.sona3 = list(sona)
+                mangu_objekt.save()
+                mitmes_taht = 0
+                for taht in mangu_objekt.sona1:
+                    if taht == list(oige_sona)[mitmes_taht]:
+                        (mangu_objekt.sona1_varv).insert(mitmes_taht, "green")
+                        mangu_objekt.save()
+                        mitmes_taht += 1
+                    elif taht in list(oige_sona):
+                        (mangu_objekt.sona1_varv).insert(mitmes_taht, "yellow")
+                        mangu_objekt.save()
+                        mitmes_taht += 1
+                    else:
+                        (mangu_objekt.sona1_varv).insert(mitmes_taht, "grey")
+                        mangu_objekt.save()
+                        mitmes_taht += 1
+            else:
+                sona_pole = True
         elif mitmes == 4:
-            mangu_objekt.sona4 = sona
-            mangu_objekt.save()
+            if sona in sonade_list:
+                mangu_objekt.sona4 = list(sona)
+                mangu_objekt.save()
+                mitmes_taht = 0
+                for taht in mangu_objekt.sona1:
+                    if taht == list(oige_sona)[mitmes_taht]:
+                        (mangu_objekt.sona1_varv).insert(mitmes_taht, "green")
+                        mangu_objekt.save()
+                        mitmes_taht += 1
+                    elif taht in list(oige_sona):
+                        (mangu_objekt.sona1_varv).insert(mitmes_taht, "yellow")
+                        mangu_objekt.save()
+                        mitmes_taht += 1
+                    else:
+                        (mangu_objekt.sona1_varv).insert(mitmes_taht, "grey")
+                        mangu_objekt.save()
+                        mitmes_taht += 1
+            else:
+                sona_pole = True
         elif mitmes == 5:
-            mangu_objekt.sona5 = sona
-            mangu_objekt.save()
-        sona1 = list(mangu_objekt.sona1[0])
-        sona2 = list(mangu_objekt.sona2[0])
-        sona3 = list(mangu_objekt.sona3[0])
-        sona4 = list(mangu_objekt.sona4[0])
-        sona5 = list(mangu_objekt.sona5[0])
+            if sona in sonade_list:
+                mangu_objekt.sona5 = list(sona)
+                mangu_objekt.save()
+                mitmes_taht = 0
+                for taht in mangu_objekt.sona1:
+                    if taht == list(oige_sona)[mitmes_taht]:
+                        (mangu_objekt.sona1_varv).insert(mitmes_taht, "green")
+                        mangu_objekt.save()
+                        mitmes_taht += 1
+                    elif taht in list(oige_sona):
+                        (mangu_objekt.sona1_varv).insert(mitmes_taht, "yellow")
+                        mangu_objekt.save()
+                        mitmes_taht += 1
+                    else:
+                        (mangu_objekt.sona1_varv).insert(mitmes_taht, "grey")
+                        mangu_objekt.save()
+                        mitmes_taht += 1
+            else:
+                sona_pole = True
+        else:
+            print("MITMES ON KATKI - väärtus: "+str(mitmes))
+
+        #teeb listid tahtede ja varvidega
+        sona1 = (mangu_objekt.sona1).extend(mangu_objekt.sona1_varv)
+        sona2 = (mangu_objekt.sona2).extend(mangu_objekt.sona2_varv)
+        sona3 = (mangu_objekt.sona3).extend(mangu_objekt.sona3_varv)
+        sona4 = (mangu_objekt.sona4).extend(mangu_objekt.sona4_varv)
+        sona5 = (mangu_objekt.sona5).extend(mangu_objekt.sona5_varv)
+
+        if sona_pole:
+            sonum = "Sellist eestikeelset sõna meil pole, proovi mõnda teist!"
+
         context = {
-            "taht1_1": sona1[0],
-            "taht1_2": sona1[1],
-            "taht1_3": sona1[2],
-            "taht1_4": sona1[3],
-            "taht1_5": sona1[4],
-            "taht2_1": sona2[0],
-            "taht2_2": sona2[1],
-            "taht2_3": sona2[2],
-            "taht2_4": sona2[3],
-            "taht2_5": sona2[4],
-            "taht3_1": sona3[0],
-            "taht3_2": sona3[1],
-            "taht3_3": sona3[2],
-            "taht3_4": sona3[3],
-            "taht3_5": sona3[4],
-            "taht4_1": sona4[0],
-            "taht4_2": sona4[1],
-            "taht4_3": sona4[2],
-            "taht4_4": sona4[3],
-            "taht4_5": sona4[4],
-            "taht5_1": sona5[0],
-            "taht5_2": sona5[1],
-            "taht5_3": sona5[2],
-            "taht5_4": sona5[3],
-            "taht5_5": sona5[4],
+            "sona1": sona1,
+            "sona2": sona2,
+            "sona3": sona3,
+            "sona4": sona4,
+            "sona5": sona5,
+            "sonum": sonum,
         }
         if sona == oige_sona:
-            sonum="Õige!"
-            return render(request, "wordle/pealeht.html", {"varv":"green", "sonum":sonum, "kas":True}.update(context))
+            return render(request, "wordle/pealeht.html", context)
             mangu_objekt.delete()
         else:
-            return render(request, "wordle/pealeht.html", {"varv":"red", "sonum":"Proovi uuesti!", "kas":False}.update(context))
+            return render(request, "wordle/pealeht.html", context)
     elif request.method=="GET":
         return HttpResponseRedirect(reverse("algus"))
