12d2a4042e4b7a70e03825bea26abf999dfeb756
Kristjan-Ai <kristjan.arusalu001@gmail.com>
2023-06-12 20:59:34 +0300

    ma parandasin sonu

--- parim_wordle_universumis/Sõnade_loend/tee_sonastik.py
+++ parim_wordle_universumis/Sõnade_loend/tee_sonastik.py
@@ -2,9 +2,10 @@ f1 = open("sonad.txt", "r", encoding="iso_8859_1")
 f2 = open("valmis_sonad.py", "w", encoding="utf-8")
 tekst = f1.readlines()
 sonade_list = []
+tähestik = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r",]
 for rida in tekst:
     rida = rida.strip().lower()
-    if len(rida) == 5 and "-" not in rida:
+    if len(rida) == 5 and rida.isalpha() and rida not in sonade_list:
         sonade_list.append(rida)
 f2.write("sonade_list = "+str(sonade_list))
 f1.close()
